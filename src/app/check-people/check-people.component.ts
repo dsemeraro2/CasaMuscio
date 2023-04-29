b99f58f94304b73ccef915ea459159e5f38189e8
dsemeraro2 <daniele.semerarods@libero.it>
2023-04-30 01:57:55 +0200

    WIP

--- src/app/check-people/check-people.component.ts
+++ src/app/check-people/check-people.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-check-people',
   templateUrl: './check-people.component.html',
-  styleUrls: ['./check-people.component.css']
+  styleUrls: ['./check-people.component.css'],
 })
 export class CheckPeopleComponent {
-
+  statusAlessandro = 'Online';
+  statusCarmine = 'Offline';
+  statusDaniele = 'Offline';
+  statusNicolas = 'Online';
 }
