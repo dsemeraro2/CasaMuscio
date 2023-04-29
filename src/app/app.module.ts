426c476be9725f08a8f68c38fedf5377bd548431
dsemeraro2 <daniele.semerarods@libero.it>
2023-04-30 01:33:02 +0200

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppRoutes } from './app.routes';
 import { RouterModule } from '@angular/router';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { CountExpressComponent } from './count-express/count-express.component';
+import { CheckPeopleComponent } from './check-people/check-people.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ErrorPageComponent,
-    CountExpressComponent
+    CountExpressComponent,
+    CheckPeopleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/check-people/check-people.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check-people',
+  templateUrl: './check-people.component.html',
+  styleUrls: ['./check-people.component.css']
+})
+export class CheckPeopleComponent {
+
+}
