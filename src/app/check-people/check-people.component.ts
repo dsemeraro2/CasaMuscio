83cce53b2cb71b0b66244ef8091bc53cfe9975a7
dsemeraro2 <daniele.semerarods@liberolit>
2023-04-30 12:11:01 +0200

    WIP

--- src/app/check-people/check-people.component.ts
+++ src/app/check-people/check-people.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-check-people',
   templateUrl: './check-people.component.html',
   styleUrls: ['./check-people.component.css'],
 })
-export class CheckPeopleComponent {
-  statusAlessandro = 'Online';
-  statusCarmine = 'Offline';
-  statusDaniele = 'Offline';
-  statusNicolas = 'Online';
-
-  reload(){
-    console.log("Ricaricato!")
+export class CheckPeopleComponent implements OnInit {
+  ngOnInit() {
+    this.reload();
   }
 
+  statusAlessandro = 'Loading';
+  statusCarmine = 'Loading';
+  statusDaniele = 'Loading';
+  statusNicolas = 'Loading';
+
+  reload() {
+    console.log('Ricaricato!');
+    this.statusAlessandro = 'Offline';
+    this.statusCarmine = 'Offline';
+    this.statusDaniele = 'Online';
+    this.statusNicolas = 'Online';
+  }
 }
