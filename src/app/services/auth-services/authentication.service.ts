1d153bdc6bedfb4dd9bfcce48c041d7b8a1c1f9a
dsemeraro2 <daniele.semerarods@libero.it>
2023-05-04 21:49:09 +0200

    WIP

--- src/app/services/auth-services/authentication.service.ts
+++ src/app/services/auth-services/authentication.service.ts
@@ -2,20 +2,23 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
   private authenticated: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   login(username: string, password: string): Observable<boolean> {
-    // Simulate authentication
-    if (username === 'admin' && password === 'admin') {
+    if (username === '' && password === '') {
+      alert("Username e/o password errati!")
+    }
+
+    if (username === 'imuscino' && password === 'viapiave128') {
       this.authenticated = true;
       return of(true);
     } else {
-        alert("Username e/o password errati!")
+      alert('Username e/o password errati!');
       return of(false);
     }
   }
