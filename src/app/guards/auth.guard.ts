d6f4afe7f09ce3b328f4011bed913e0b7b93fe4d
dsemeraro2 <daniele.semerarods@libero.it>
2023-05-04 21:23:28 +0200

    WIP

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import {
+  CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   UrlTree,
   Router,
-  CanActivate,
+  UrlSegment
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../services/auth-services/authentication.service';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   constructor(
@@ -19,13 +20,9 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   canActivate(
-    next: ActivatedRouteSnapshot,
+    route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
+  ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     if (this.authService.isAuthenticated()) {
       return true;
     } else {
